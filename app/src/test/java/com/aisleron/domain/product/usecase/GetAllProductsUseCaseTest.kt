8dc1eefb1b99b9d2550a3483c4a809fc5c23b906
danie <danie@roux.one>
2024-05-17 16:10:08 +1200

    Added GetAllProductUseCase Tests

---
+++ app/src/test/java/com/aisleron/domain/product/usecase/GetAllProductsUseCaseTest.kt
@@ -0,0 +1,38 @@
+package com.aisleron.domain.product.usecase
+
+import com.aisleron.data.TestDataManager
+import com.aisleron.domain.product.Product
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class GetAllProductsUseCaseTest {
+
+    private lateinit var testData: TestDataManager
+    private lateinit var getAllProductsUseCase: GetAllProductsUseCase
+
+    @BeforeEach
+    fun setUp() {
+        testData = TestDataManager()
+        getAllProductsUseCase = GetAllProductsUseCase(testData.productRepository)
+    }
+
+    @AfterEach
+    fun tearDown() {
+    }
+
+    @Test
+    fun getAllProducts_ProductsReturned_MatchesRepoList() {
+        val getProductsList: List<Product>
+        val repoProductsList: List<Product>
+
+        runBlocking {
+            repoProductsList = testData.productRepository.getAll()
+            getProductsList = getAllProductsUseCase()
+        }
+
+        assertEquals(repoProductsList, getProductsList)
+    }
+}
