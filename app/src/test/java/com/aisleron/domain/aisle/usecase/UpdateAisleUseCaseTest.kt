7a1056445d2deb28f65bb6e2550f61c075840f75
danie <danie@roux.one>
2024-05-19 12:41:52 +1200

    Added UpdateAisleUseCase Tests

---
+++ app/src/test/java/com/aisleron/domain/aisle/usecase/UpdateAisleUseCaseTest.kt
@@ -0,0 +1,65 @@
+package com.aisleron.domain.aisle.usecase
+
+import com.aisleron.data.TestDataManager
+import com.aisleron.domain.aisle.Aisle
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class UpdateAisleUseCaseTest {
+    private lateinit var testData: TestDataManager
+    private lateinit var updateAisleUseCase: UpdateAisleUseCase
+    private lateinit var existingAisle: Aisle
+
+    @BeforeEach
+    fun setUp() {
+        testData = TestDataManager()
+
+        existingAisle = runBlocking { testData.aisleRepository.get(1)!! }
+
+        updateAisleUseCase = UpdateAisleUseCase(testData.aisleRepository)
+    }
+
+    @Test
+    fun updateAisle_AisleExists_RecordUpdated() {
+        val updateAisle = existingAisle.copy(name = existingAisle.name + " Updated")
+        val updatedAisle: Aisle?
+        val countBefore: Int
+        val countAfter: Int
+        runBlocking {
+            countBefore = testData.aisleRepository.getAll().count()
+            updateAisleUseCase(updateAisle)
+            updatedAisle = testData.aisleRepository.get(existingAisle.id)
+            countAfter = testData.aisleRepository.getAll().count()
+        }
+        Assertions.assertNotNull(updatedAisle)
+        Assertions.assertEquals(countBefore, countAfter)
+        Assertions.assertEquals(updateAisle, updatedAisle)
+    }
+
+    @Test
+    fun updateAisle_AisleDoesNotExist_RecordCreated() {
+        val newAisle = existingAisle.copy(
+            id = 0,
+            name = existingAisle.name + " Inserted",
+            isDefault = false
+        )
+        val updatedAisle: Aisle?
+        val countBefore: Int
+        val countAfter: Int
+        runBlocking {
+            countBefore = testData.aisleRepository.getAll().count()
+            updateAisleUseCase(newAisle)
+            val id = testData.aisleRepository.getAll().maxOf { it.id }
+            updatedAisle = testData.aisleRepository.get(id)
+            countAfter = testData.aisleRepository.getAll().count()
+        }
+        Assertions.assertNotNull(updatedAisle)
+        Assertions.assertEquals(countBefore + 1, countAfter)
+        Assertions.assertEquals(newAisle.name, updatedAisle?.name)
+        Assertions.assertEquals(newAisle.locationId, updatedAisle?.locationId)
+        Assertions.assertEquals(newAisle.rank, updatedAisle?.rank)
+        Assertions.assertEquals(newAisle.isDefault, updatedAisle?.isDefault)
+    }
+}
