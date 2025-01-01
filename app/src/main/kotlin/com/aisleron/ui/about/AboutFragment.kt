eb3618890d5f934532a791a2ade6fd7684813970
danie <danie@roux.one>
2025-01-01 15:55:16 +1300

    Add web links on About page

--- app/src/main/kotlin/com/aisleron/ui/about/AboutFragment.kt
+++ app/src/main/kotlin/com/aisleron/ui/about/AboutFragment.kt
@@ -16,14 +16,13 @@ class AboutFragment : PreferenceFragmentCompat() {
         SOURCE_CODE("about_support_sourcecode"),
         LICENSE("about_legal_license"),
         PRIVACY("about_legal_privacy"),
-        TERMS_AND_CONDITIONS("about_legal_tnc")
     }
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.about, rootKey)
 
         findPreference<Preference>(AboutOption.VERSION.key)?.let {
-            it.summary = aboutViewModel.versionName
+            it.summary = getString(R.string.about_support_version_summary, aboutViewModel.versionName)
         }
     }
 }
--- app/src/main/kotlin/com/aisleron/ui/about/AboutViewModel.kt
+++ app/src/main/kotlin/com/aisleron/ui/about/AboutViewModel.kt
@@ -7,11 +7,4 @@ class AboutViewModel : ViewModel() {
 
     val versionName: String get() = BuildConfig.VERSION_NAME
 
-    /**
-     * Report an issue
-     * Source code
-     * License
-     * Privacy Policy
-     * Terms and Conditions
-     */
 }
