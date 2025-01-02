491ae3f28a58abd867585ec78368b44b7a345c1a
danie <danie@roux.one>
2025-01-02 21:23:27 +1300

    Navigation Drawer: Change 'In Stock' icon and add 'Home' title; Add auto-naming of apk to gradle script

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,8 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 1
-        versionName = "0.1.0"
+        versionName = "0.2.0"
+        base.archivesName = "$applicationId-$versionName"
 
         testInstrumentationRunner = "com.aisleron.di.KoinInstrumentationTestRunner"
 
