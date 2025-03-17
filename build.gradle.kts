2502f9a69be5ba5101a2062510bc5435bfb4a091
danie <github@roux.one>
2025-03-17 16:41:03 +1300

    Remove unnecessary datastore-preferences dependency; Add dependency-analysis gradle plugin; Address GH-1: Change 'In Stock' Nav Bar option to 'In Stock at Home'

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,13 @@ plugins {
     id("com.google.devtools.ksp") version "2.1.0-1.0.29" apply false
     id("org.jetbrains.kotlin.plugin.parcelize") version "1.9.23" apply false
     id("com.android.library") version "8.9.0" apply false
+
+    id("com.autonomousapps.dependency-analysis") version "2.12.0"
+    // To check dependencies, run: ./gradlew buildHealth
+}
+
+dependencyAnalysis {
+    structure {
+        ignoreKtx(true) // default is false
+    }
 }
