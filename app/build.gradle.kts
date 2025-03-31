ddbe02b1849fbb090b66801ad7a6e89bb5256474
danie <github@roux.one>
2025-03-31 17:13:33 +1300

    Update version to 2025.4.2 Move signing keystore file lookup inside signingConfig in app build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,18 +29,20 @@ plugins {
 
 apply("../gradle/jacoco.gradle")
 
-// Create a variable called keystorePropertiesFile, and initialize it to your
-// keystore.properties file, in the rootProject folder.
-val keystorePropertiesFile = rootProject.file("keystore.properties")
 
-// Initialize a new Properties() object called keystoreProperties.
-val keystoreProperties = Properties()
-
-// Load your keystore.properties file into the keystoreProperties object.
-keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 
 android {
     signingConfigs {
+        // Create a variable called keystorePropertiesFile, and initialize it to your
+        // keystore.properties file, in the rootProject folder.
+        val keystorePropertiesFile = rootProject.file("keystore.properties")
+
+        // Initialize a new Properties() object called keystoreProperties.
+        val keystoreProperties = Properties()
+
+        // Load your keystore.properties file into the keystoreProperties object.
+        keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+
         create("release") {
             keyAlias = keystoreProperties["keyAlias"] as String
             keyPassword = keystoreProperties["keyPassword"] as String
@@ -56,7 +58,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 4
-        versionName = "2025.4.1"
+        versionName = "2025.4.2"
         base.archivesName = "$applicationId-$versionName"
 
         testInstrumentationRunner = "com.aisleron.di.KoinInstrumentationTestRunner"
