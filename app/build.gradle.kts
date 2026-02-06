8a51ebf34db8c9049d75c9b6aea1cfbe0eefa086
Leonardo <leonardo.guilherme@gmail.com>
2026-02-06 14:37:11 -0300

    Add check for keystore.properties existence before creating the release signing configuration.
    Signed-off-by: Leonardo <leonardo.guilherme@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,21 +55,18 @@ android {
     }
 
     signingConfigs {
-        // Create a variable called keystorePropertiesFile, and initialize it to your
-        // keystore.properties file, in the rootProject folder.
         val keystorePropertiesFile = rootProject.file("keystore.properties")
 
-        // Initialize a new Properties() object called keystoreProperties.
-        val keystoreProperties = Properties()
+        if (keystorePropertiesFile.exists()) {
+            val keystoreProperties = Properties()
+            keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 
-        // Load your keystore.properties file into the keystoreProperties object.
-        keystoreProperties.load(FileInputStream(keystorePropertiesFile))
-
-        create("release") {
-            keyAlias = keystoreProperties["keyAlias"] as String
-            keyPassword = keystoreProperties["keyPassword"] as String
-            storeFile = file(keystoreProperties["storeFile"] as String)
-            storePassword = keystoreProperties["storePassword"] as String
+            create("release") {
+                keyAlias = keystoreProperties["keyAlias"] as String
+                keyPassword = keystoreProperties["keyPassword"] as String
+                storeFile = file(keystoreProperties["storeFile"] as String)
+                storePassword = keystoreProperties["storePassword"] as String
+            }
         }
     }
     namespace = "com.aisleron"
@@ -101,7 +98,7 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
-            signingConfig = signingConfigs.getByName("release")
+            signingConfig = signingConfigs.findByName("release")
         }
 
         debug {
