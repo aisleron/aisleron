f2d93694f7b2640b1288538852dcad1275e0b148
thebatdan <github@roux.one>
2026-06-15 19:32:48 +1200

    Add Portuguese to language selection.
    Signed-off-by: thebatdan <github@roux.one>

--- app/src/androidTest/java/com/aisleron/ui/settings/SettingsFragmentTest.kt
+++ app/src/androidTest/java/com/aisleron/ui/settings/SettingsFragmentTest.kt
@@ -443,6 +443,7 @@ class SettingsFragmentTest : KoinTest {
             Pair(R.string.language_french_fr, "fr"),
             Pair(R.string.language_italian_it, "it"),
             Pair(R.string.language_polish_pl, "pl"),
+            Pair(R.string.language_portuguese_pt, "pt"),
             Pair(R.string.language_russian_ru, "ru"),
             Pair(R.string.language_swedish_sv, "sv"),
             Pair(R.string.language_turkish_tr, "tr"),
